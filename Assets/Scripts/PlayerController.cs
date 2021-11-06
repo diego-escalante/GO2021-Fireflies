6c4a9f1eabbe950c553d2a7a74ec03d0e033d9cd
Diego Escalante <diego.escalante01@gmail.com>
2021-11-06 12:18:49 -0600

    Add coyote time and jump buffer

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,15 +6,17 @@ public class PlayerController : MonoBehaviour {
     [SerializeField] private float JumpHeight = 0.6f;
     [SerializeField] private float gravity = -9.8f;
     [SerializeField] private float mouseSensitivity = 200f;
-    
+    [SerializeField] private float coyoteTime = 0.25f;
+    [SerializeField] private float jumpBufferTime = 0.1f;
     
     private float jumpVelocity;
-    
     private Vector3 velocity;
     private Vector3 targetLookRotation;
     private CharacterController controller;
     private Transform camTrans;
     private float xRotation;
+    private float coyoteTimeLeft;
+    private float jumpBufferTimeLeft;
     
     private void Awake() {
         controller = GetComponent<CharacterController>();
@@ -25,7 +27,6 @@ public class PlayerController : MonoBehaviour {
     }
     
     private void Update() {
-        
         // Handle body rotation. (Looking Left/Right)
         Vector3 bodyRotation = transform.eulerAngles;
         bodyRotation.y += Input.GetAxis("Mouse X") * mouseSensitivity;
@@ -35,12 +36,35 @@ public class PlayerController : MonoBehaviour {
         xRotation = Mathf.Clamp(xRotation - Input.GetAxis("Mouse Y"), -80, 80);
         camTrans.localRotation = Quaternion.Euler(xRotation, 0, 0);
 
-        // Handle vertical movement. (Jumping, Falling, Grounded)
+        // Handle grounding.
         if (controller.isGrounded) {
-            velocity.y = Input.GetButtonDown("Jump") ? jumpVelocity : 0;
+            coyoteTimeLeft = coyoteTime;
         }
-        velocity.y += gravity * Time.deltaTime;
         
+        // Manage jump buffer time.
+        jumpBufferTimeLeft = Input.GetButtonDown("Jump") ? jumpBufferTime : jumpBufferTimeLeft - Time.deltaTime;
+
+        // Handle jumping.
+        if (coyoteTimeLeft >= 0) {
+            if (jumpBufferTimeLeft >= 0) {
+                // Handle jumping if in coyote time and want to jump.
+                velocity.y = jumpVelocity;
+                coyoteTimeLeft = -1;
+                jumpBufferTimeLeft = -1;
+            } else {
+                // Stay vertically still if in coyote time and not jumping.
+                velocity.y = 0;
+            }
+        }
+        
+        // Handle falling.
+        // TODO: This makes the player drift ever so slightly downwards while in coyote time. Not ideal, but
+        // vertical velocity needs to always be applied downwards for the controller to know if it's grounded.
+        velocity.y += gravity * Time.deltaTime; 
+        
+        // Update coyote time.
+        coyoteTimeLeft -= Time.deltaTime;
+
         // Handle horizontal movement. (Forwards, Backwards, Strafing)
         velocity.x = Input.GetAxisRaw("Horizontal") * moveSpeed;
         velocity.z = Input.GetAxisRaw("Vertical") * moveSpeed;
